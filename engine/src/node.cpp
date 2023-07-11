a18703cd831307441924313f215491181f7aa7d7
tam201197 <tam201197@gmail.com>
2023-07-11 11:28:07 +0200

    fixed depth

--- engine/src/node.cpp
+++ engine/src/node.cpp
@@ -1265,7 +1265,7 @@ float Node::minimax_with_depth(StateObj* state, uint8_t depth, float alpha, floa
 void Node::store_minimax_value(StateObj* state)
 {   
     lock();
-    minimaxValue = minimax_with_depth(state, 0, -2.0, -2.0, true);
+    minimaxValue = minimax_with_depth(state, 2, -2.0, -2.0, true);
     unlock();
 }
 
