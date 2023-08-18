fef5c915597614732860f2be05041f318671b4c3
QueensGambit <curry-berry@freenet.de>
2023-08-18 13:09:38 +0200

    add print()

--- engine/src/searchthread.cpp
+++ engine/src/searchthread.cpp
@@ -634,6 +634,9 @@ int pvs(StateObj* state, uint8_t depth, int alpha, int beta, const SearchSetting
         state->do_action(action);
         isBoardOk = state->is_board_ok();
         value = max(value, -pvs(state, depth - 1, -beta, -alpha, searchSettings, idxDummy, pLine, pLineIdx + 1));
+        if (depth == 2) {
+            info_string(StateConstants::action_to_uci(action, false), "returned value:", value);
+        }
         state->undo_action(action);
         if (alpha < value) {
             if (saveIndex < pLine.size()) {
