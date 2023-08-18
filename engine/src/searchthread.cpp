499948d425a944cc8b1938b73627dcdde08c76ec
QueensGambit <curry-berry@freenet.de>
2023-08-18 14:15:31 +0200

    fix

--- engine/src/searchthread.cpp
+++ engine/src/searchthread.cpp
@@ -641,7 +641,7 @@ int pvs(StateObj* state, uint8_t depth, int alpha, int beta, const SearchSetting
         value = -pvs(state, depth - 1, -beta, -alpha, searchSettings, idxDummy, pLine, pLineIdx + 1);
         //value = max(value, retValue);
         if (depth == 2) {
-            info_string(StateConstants::action_to_uci(action, false), "returned value:", retValue);
+            info_string(StateConstants::action_to_uci(action, false), "returned value:", value);
         }
         state->undo_action(action);
         if (value >= beta) return beta;
