f37c902748a4e8edab34fcdbc024a015ef2ff39d
QueensGambit <curry-berry@freenet.de>
2023-08-18 16:43:26 +0200

    update pvs()

--- engine/src/searchthread.cpp
+++ engine/src/searchthread.cpp
@@ -640,10 +640,10 @@ int pvs(StateObj* state, uint8_t depth, int alpha, int beta, const SearchSetting
         childIdx += 1;
         state->do_action(action);
         isBoardOk = state->is_board_ok();
-        value = -pvs(state, depth - 1, -beta, -alpha, searchSettings, idxDummy, &line, pLineIdx + 1);
-        //value = max(value, retValue);
+        int retValue = -pvs(state, depth - 1, -beta, -alpha, searchSettings, idxDummy, &line, pLineIdx + 1);
+        value = max(value, retValue);
         if (depth == 2) {
-            info_string(StateConstants::action_to_uci(action, false), "returned value:", value);
+            info_string(StateConstants::action_to_uci(action, false), "returned value:", retValue);
         }
         state->undo_action(action);
         //if (value >= beta) return beta;
