9c6768b50d6b5b186c904aa3f02bc343fc9dd9b3
tam201197 <tam201197@gmail.com>
2023-08-19 22:58:59 +0200

    test

--- engine/src/searchthread.cpp
+++ engine/src/searchthread.cpp
@@ -647,11 +647,17 @@ int pvs(StateObj* state, uint8_t depth, int alpha, int beta, const SearchSetting
     //int value = INT_MIN;
     for (Action action : state->legal_actions()) {
         childIdx += 1;
+        string fen_after_do_action = state->fen();
         state->do_action(action);
+        //isBoardOk = state->is_board_ok();
         int value = -pvs(state, depth - 1, -beta, -alpha, searchSettings, idxDummy, &line, pLineIdx + 1);
         //value = max(value, retValue);
         if (depth == 2) {
             info_string(StateConstants::action_to_uci(action, false), "returned value:", value);
+        }
+        state->undo_action(action);
+        if (fen_after_do_action != state->fen()) {
+            info_string("STATE AFTER UNDO IS NOT MATCHED BEFORE DO ACTION ");
         }
         //if (value >= beta) return beta;
         if (alpha < value) {
