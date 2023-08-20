c35b88d2068a0ab33e41879ea131b45df48c2a34
tam201197 <tam201197@gmail.com>
2023-08-21 00:20:41 +0200

    test

--- engine/src/searchthread.cpp
+++ engine/src/searchthread.cpp
@@ -599,7 +599,7 @@ size_t get_random_depth()
 
 
 
-int pvs(StateObj* state, uint8_t depth, int alpha, int beta, const SearchSettings* searchSettings, ChildIdx& idx, LINE * pLine, uint8_t pLineIdx)
+int pvs(StateObj* state, uint8_t depth, int alpha, int beta, const SearchSettings* searchSettings, ChildIdx& idx, LINE* pLine, uint8_t pLineIdx)
 {
     LINE line;
     line.cmove = 0;
@@ -646,7 +646,7 @@ int pvs(StateObj* state, uint8_t depth, int alpha, int beta, const SearchSetting
         childIdx += 1;
         string fen_after_do_action = state->fen();
         state->do_action(action);
-        int value = -pvs(state, depth - 1, -beta, -alpha, searchSettings, idxDummy, pLine, pLineIdx + 1);
+        int value = -pvs(state, depth - 1, -beta, -alpha, searchSettings, idxDummy, &line, pLineIdx + 1);
         //value = max(value, retValue);
         if (depth == 2) {
             info_string(StateConstants::action_to_uci(action, false), "returned value:", value);
@@ -658,8 +658,6 @@ int pvs(StateObj* state, uint8_t depth, int alpha, int beta, const SearchSetting
         //if (value >= beta) return beta;
         if (alpha <= value) {
             alpha = value;
-            info_string("depth:", int(depth));
-            info_string("update alpha to:", alpha);
             idx = childIdx;
             pLine->argmove[0] = action;
             memcpy(pLine->argmove + 1, line.argmove, line.cmove * sizeof(Action));
