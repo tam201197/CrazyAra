bbc142ee186c255ce58208568cdf1c9e42a90f17
tam201197 <tam201197@gmail.com>
2023-08-22 16:04:05 +0200

    test with depth 3

--- engine/src/searchthread.cpp
+++ engine/src/searchthread.cpp
@@ -183,7 +183,7 @@ Node* SearchThread::get_new_child_to_evaluate(NodeDescription& description)
     if (searchSettings->mctsIpM && pLine.empty()) {
         unique_ptr<StateObj> evalState = unique_ptr<StateObj>(rootState->clone());
         currentNode->lock();
-        childIdx = minimax_select_child_node(evalState.get(), currentNode, 4);
+        childIdx = minimax_select_child_node(evalState.get(), currentNode, 3);
         currentNode->setIsMinimaxCalled(true);
         currentNode->unlock();
         pLine.clear();
