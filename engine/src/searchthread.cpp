d105137a94b6586f5a1c2c18a0a759e8d9626d4b
tam201197 <tam201197@gmail.com>
2023-09-28 12:20:38 +0200

    removed prior_weight, added pv-Line

--- engine/src/searchthread.cpp
+++ engine/src/searchthread.cpp
@@ -202,9 +202,11 @@ Node* SearchThread::get_new_child_to_evaluate(NodeDescription& description)
                     childIdx = minimax_select_child_node(evalState.get(), currentNode, searchSettings->minimaxDepth, pTempLine, minimaxValue);
                     currentNode->increase_minimax_count();
                     nextNode = currentNode->get_child_node(childIdx);
+                    /*
                     if (minimaxValue > -2.0 && nextNode != nullptr) {
                         nextNode->update_qValue_after_minimax_search(currentNode, childIdx, minimaxValue, searchSettings);
                     }
+                    */
                     //pLine.pop_front();
                 }
                 else if (numberVisits >= 5000 && pTempLine.empty() && currentNode->get_minimax_count() < 2) {
@@ -344,12 +346,9 @@ ChildIdx SearchThread::minimax_select_child_node(StateObj* state, Node* node, ui
     if (currNode == nullptr) {
         return childIdx;
     }
-    else {
-        minimaxValue = - currNode->get_init_value();
-    }
-    /*for (int i = 1; i < line.cmove; i++) {
+    for (int i = 1; i < line.cmove; i++) {
         pTempLine.emplace_back(line.argmove[i]);
-    }*/
+    }
     /*
     for (int i = 1; i < line.cmove; i++) {
         currNode->lock();
